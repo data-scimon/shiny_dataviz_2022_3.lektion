01a2966ef7df27a6949d68d8878455f918ab4413
data-scimon <simonmj91@hotmail.com>
2022-01-18 11:55:47 +0100

    RT

--- app.R
+++ app.R
@@ -3,9 +3,14 @@ library(tidyverse)
 library(readxl)
 library(shinythemes)
 
+
 passat <- read_excel("passat.xlsx")
+rt <- read_csv2("Rt_cases.csv")
+
+
 theme_set(theme_minimal())
 options(scipen = 999)
+
 
 
 
@@ -66,7 +71,18 @@ ui <- fluidPage(
                                              multiple = FALSE),
                                  br(),
                                  plotOutput("plot")),
-                        tabPanel("Tabel", tableOutput("tabel")))
+                        
+                        tabPanel("Tabel", tableOutput("tabel")),
+                        
+                        tabPanel("Corona-plot",
+                                 br(),
+                                 dateRangeInput(inputId = "rt_date", "Vælg datoer",
+                                                start = "2020-04-01",
+                                                end = "2020-05-01",
+                                                max = "Sys.Date()",
+                                                startview = "month",
+                                                weekstart = 1),
+                                 plotOutput("covid_plot")))
             
         )
     )
@@ -77,10 +93,17 @@ server <- function(input, output) {
     
     output$plot <- renderPlot({
         
-        ggplot(data = passat, aes_string(x = as.name(input$xaxis), y = as.name(input$yaxis))) +
+        passat_adj <- passat %>%
+            filter(price <= input$price_adj)
+        
+        ggplot(data = passat_adj, aes_string(x = as.name(input$xaxis), y = as.name(input$yaxis))) +
             geom_point(size = input$size, alpha = input$alpha) +
             labs(title = paste0("Her er sammenhængen mellem ", as.name(input$xaxis),
-                                " og ", as.name(input$yaxis)))
+                                " og ", as.name(input$yaxis)),
+                 subtitle = paste0("Her vises biler der koster mindre end ", input$price_adj, " kroner")) +
+            theme(plot.title = element_text(size = 30),
+                  plot.subtitle = element_text(size = 20),
+                  axis.title = element_text(size = 20))
         
     })
     
@@ -90,6 +113,22 @@ server <- function(input, output) {
             filter(price <= input$price_adj)
         
         print(pas_adj)
+        
+    })
+    
+    output$covid_plot <- renderPlot({
+        
+        rt2 <- rt %>%
+            
+            filter(SampleDate >= as.Date(input$rt_date[1]) & SampleDate <= as.Date(input$rt_date[2]))
+        
+        p <- ggplot(data = rt2, aes(x = as.Date(SampleDate), y = estimate)) +
+            geom_line() +
+            geom_hline(yintercept = 1, color = "red", size = 2, alpha = 0.5) +
+            labs(x = "Dato",
+                 y = "Kontakttal")
+        
+        print(p)
         
     })
     
